26e468a928c18a06180ac4dd4894c680f9205054
zhoujin <jin100chow@hotamil.com>
2018-04-13 23:55:44 +0800

    model post controller new button

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:group_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path ,
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:group_id])
+  end
+
+  def update
+    @post = Post.find(params[:group_id])
+   if  @post.update(post_params)
+    redirect_to posts_path ,
+   else
+    render :edit
+   end
+  end
+
+   def destroy
+     @post = Post.find(params[:group_id])
+     @post.delete
+   end
+
+end
