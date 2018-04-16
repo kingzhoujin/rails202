6e6718abd3eedda136ae2f55ce45380f4ebaac05
zhoujin <jin100chow@hotamil.com>
2018-04-16 16:50:18 +0800

    add account post implement post edit destroy

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,4 +4,19 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to group_post_path
+  end
+
+
 end
